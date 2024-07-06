0870ea03535834f04fb9c0028fc8e66d5565a568
ANKITA7722 <ANKITA7722>
2024-07-06 19:16:20 +0100

    js questions

---
+++ file-1.js
@@ -0,0 +1,117 @@
+// let age = parseInt(prompt("enter your age"))
+// if(age>=18 )
+// {
+//     let ind = parseInt(prompt("press 1 for india"))
+//     if(ind==1)
+//     {
+//         alert("eligible");
+// } 
+// else{
+//     alert("not eligible");
+// } 
+// }
+// else
+// {
+//     alert("pahle bade jao");
+// } 
+
+
+
+//que-2
+
+
+/*var n = parseInt(prompt("enter any no"))
+switch (n) {
+
+    case 1:
+        alert("devendra");
+        break;
+    case 2:
+        alert("ankita");
+        break;
+    case 3:
+        alert("nikita");
+        break;
+    case 4:
+        alert("nasmin");
+        break;
+    case 5:
+        alert("khushbu");
+        break;
+    case 6:
+        alert("ansika");
+        break;
+    case 7:
+        alert("xyz");
+        break;
+
+    default:
+        alert("me kisi ko nhi janti");
+}*/
+
+//question-3
+/*var a = parseInt(prompt("enter any no"))
+var b = parseInt(prompt("enter any no"))
+var  c = parseInt(prompt("select 1 for * and 2 for  and 3 for devide nd 4 for substaraction"))
+switch (c) {
+
+    case 1:
+        alert (a*b);
+        break;
+    case 2:
+        alert(a+b);
+        break;
+    case 3:
+        alert(a/b);
+        break;
+    case 4:
+        alert(a-b);
+        break;
+
+    default:
+        alert("invalid");
+}*/
+//que-4
+/*var n = 1;
+do
+{
+   
+   document.write (n);
+   n++;
+}
+while(n<=10);*/
+
+//write a program to print the table
+var n = 10;
+
+/*do
+{
+   
+   document.write (n);
+   n--//;
+}
+while(n>=1);*/
+
+
+var n = 10;
+do
+{
+   
+   document.write (n*m);
+   m--//;
+}
+while(m>=1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ file-2.js
@@ -0,0 +1,66 @@
+//var a = parseInt(prompt("enter any no."))
+//alert(a*a*a)
+//alert(a*a)
+//var b = parseInt(prompt("enter any no."))
+//var  h= parseInt(prompt("enter any no."))
+//alert(1/2*b*h)
+/*let n = parseInt(prompt("enter any no."))
+if(n%2!=0)
+{
+    alert("odd");
+}
+else
+{
+    alert("even");
+}*/
+
+/*let n = parseInt(prompt("enter any no."))
+if(n>0)
+{
+    alert("positiv");
+}
+ else if(n<0)
+{
+    alert("negetiv");
+}
+else
+{
+    alert("nuteral")
+}*/
+
+/*let n = parseInt(prompt("enter any no."))
+if(n>=100 && n<=500)
+{
+    alert("5 star");
+}
+ else if(n>500 && n<=1000)
+{
+    alert("shopping");
+}
+else if(n<1000 && n<10000)
+{
+    alert("trip")
+}
+else
+{
+    alert("no money")
+}*/
+let n = parseInt(prompt("enter any no."))
+if(n%2==0)
+{
+    alert("even");
+
+}
+ else if(n*n)
+{
+    alert("shopping");
+}
+else if(n<1000 && n<10000)
+{
+    alert("trip")
+}
+else
+{
+    alert("no money")
+}
+
