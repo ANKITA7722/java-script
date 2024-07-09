61ea42799e112686909aa29d5571820ae9c894a9
ANKITA7722 <ANKITA7722>
2024-07-09 18:18:20 +0100

    nestedforloopcovered

---
+++ loop.js
@@ -0,0 +1,41 @@
+/*let n = 1;
+while(n<=10)
+{
+   document.write(n+"<br>")
+   n++;
+}*/
+//write a program to print odd Number beetwen 1 to 20 using while loop
+ /* let n = 1;
+while(n<=20)
+{
+    if(n%2!=0)
+  {
+     document.write(n+"<br>")
+  }
+   n++;
+  
+}*/
+//write a program to print squares of Numbers beetwin 15 to 1
+/*let n = 1;
+while(n<=15)
+{
+     document.write(n*n+"<br>")
+
+   n--;
+  
+}*/
+//for loop:
+
+/*for( let n=1; n<=10; n++)
+    {
+    alert(n+"<br>")
+    }*/
+  //  write a program to print a table 1 to 10 
+  let n = parseInt(prompt("enter any no."))
+  let sum;
+  for( n; n>0; n=n/10)
+      {
+        sum=sum+1;
+      alert(sum)
+      
+      }
---
+++ nestedforloop.js
@@ -0,0 +1,24 @@
+// // let p=0
+// // for(let r=1;r<=5;r++)
+// // {
+// // for( let c=1;c<=r;c++){
+// //     p++
+// //        document.write( p);
+       
+// // }
+// // document.write("<br>")
+
+// // }
+
+
+// let a = '5';
+// let b = 5;
+// console.long(a==b)
+
+// let n = 29;
+// (n<=15)?document.write("great"):document.write("small");
+//write a program to check  number is even or odd
+ let n = parseInt(prompt("enter any no."));
+(n%2==0)?console.long("even"):console.long("odd");
+
+
