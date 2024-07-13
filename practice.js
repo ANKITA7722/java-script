aeeea37518d614a67acc1697e3b35acac7b7f55e
ANKITA7722 <ANKITA7722>
2024-07-13 10:59:51 +0100

    practice quetion

---
+++ practice.js
@@ -0,0 +1,59 @@
+//first mathod print karane ki for loop se 
+/*let sum = 0;
+let n =4
+for(let i=1;i<=n;i++)
+{
+    sum=sum+i
+   // document.write("ankita <br>");
+    
+}
+document.write(sum);*/
+
+//second mathhod while  loop se print karane ka
+/*let i=1
+while(i<=9)
+{
+    document.write("ankita<br>")
+    i++;
+}*/
+
+//third mathode do while loop se print karane ki
+/*let i=1
+do{
+    document.write("<br>i=" ,i)
+    i++;
+}
+while(i<9);*/
+
+//array:-for of loop:-strings and arrays ke uper loop lagane me help karta he
+//1.
+/*let a = "ankita"
+for(let i of a ) 
+{
+    document.write("<br>i=",i)
+}*/
+//2.
+/*let a = "ankita"
+let size = 0;
+for(let val of str ) ;
+{
+    document.write("<br>val=",val)
+    size++;
+}
+document.write("string size=",size);*/
+
+//for in loop
+/*let student = {
+    name : "ankita", age:22, cgpa:8.5,ispass:true};
+    for(let key in student)
+    {
+        document.write("<br>keys=",key,"value=",student[key]);
+        }*/
+
+//que:-
+for(let i=1;i<=100;i++)
+{
+    if(i%2!=0)
+    document.write("<br>odd no=",i)
+}
+//document.write("odd")
