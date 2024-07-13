9a2e6d57741f6e96a0f4e903c9721a5cb3e5434d
ANKITA7722 <ANKITA7722>
2024-07-13 09:28:32 +0100

    array

---
+++ array.js
@@ -0,0 +1,56 @@
+// var ar = [1,2,4,5, ]
+// for(i=0;i<5;i++)
+// {
+// console.log(ar[i])
+// }
+/*let n = parseInt(prompt("enter array size"))
+var ar = [ n]
+for(i=0;i<=n;i++)
+     {
+      ar[i] = parseInt(prompt("enter any no."+ i))
+      
+ }
+for(i=0;i<=n;i++)
+     {
+        //console.log(ar[i])
+        document.write(ar[i]+"<br>")
+      
+ }*/
+ //wap to add all 10 aliment 
+
+/*var ar = [1,2,3,4,5,6,7,8,9,10]
+s = 0
+for(i=0;i<10;i++)
+     {
+          s = s+ar[i]
+        
+      
+ }
+ document.write(s)*/
+ //wap to print odd nu. in 20 to 1 using array
+ 
+var ar = [ 10,9,8,7,6,5,4,3,2,1]
+for(i=0;i<=10;i++)
+     {
+      if(ar[i%2!=0])
+      {
+          document.write(ar[i]);
+      }
+      
+ }
+
+ /*1.  var ar = [ 10,9,8,7,6,5,4,3,2,1]
+//for of
+for(let v of ar)
+     {
+     
+          document.write(v);    
+ }
+
+ 2. var ar = [ 10,9,8,7,6,5,4,3,2,1]
+//for in
+for(let v in ar)
+     {
+     
+          document.write(v);    
+ }*/
