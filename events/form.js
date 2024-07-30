9bcc8574b048688232e71013ffc192845b801159
ANKITA7722 <ANKITA7722>
2024-07-30 19:11:01 +0100

    increament decreament using  function

--- events/form.js
+++ events/form.js
@@ -1,5 +1,26 @@
-function data(){
+/*function data(){
     let number1 = parseInt(document.getElementById("num1").value);
     let number2 = parseInt(document.getElementById("num2").value);
     document.write(number1+number2);
+}*/
+/*function data(){
+    alert("working");
+}
+function datatype(){
+    alert("working");
+}*/
+var v = 0;
+let c = document.getElementById("count")
+function dec (){
+    if(v>0)
+{
+    v--;
+    c.innerHTML=v
+}
+}
+function inc(){
+   v++;
+   c.innerHTML=v
+
+  
 }
