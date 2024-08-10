6a6f7b30fe60ad08a96a40596d1184b64e694103
ANKITA7722 <ANKITA7722>
2024-08-10 10:37:47 +0100

    math function

---
+++ events/new js folder/script.js
@@ -0,0 +1,18 @@
+// let x = Math.sqrt(9);//squre  returns squre rootof  a number
+// x = math.sign(787);//sign returns 1 if number is positive -1 if number is returns is nagative,
+// //0 of number is 0.
+// x = Math.pow(4,3);//return power of a number (number,power)
+// x = Math.abs(-45);//return absolute value always positive
+// x = Math.ceil(6.9)//return the above on higher value
+// x = Math.round(6.5);//its return round of value
+// x = Math.floor(6.9);//return lower value
+// x = Math.min(34,2,0,-24,99,123);//returnminimum value
+// x = Math.max(34,2,0,-24,99,123);//its returns maximum value
+// x= Math.trunc(4.2345);//removes the desimle numbers
+x = Math.random();//random number between 0(inclusive)and 1(exclusive)
+x = Math.min()*100;
+x = Math.floor(Math.random()*100);
+console.log(x);
+// alert(x);
+
+document.getElementById("demo").innerHTML = x;
